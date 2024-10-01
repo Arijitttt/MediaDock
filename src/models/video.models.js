4ad1f3970aeb19528378312d9f7858a2df9b1269
Arijitttt <abhattacharya030@gmail.com>
2024-10-01 21:12:54 +0530

    Video Model

---
+++ src/models/video.models.js
@@ -0,0 +1,50 @@
+/* 
+owner ObjectId users
+videoFile String
+thumbnil string
+title string
+description string
+duration number
+views number
+isPublished boolean
+createdAt Date
+updatedAt Date
+*/
+import mongoose,{Schema} from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile:{
+        type:String, // cloudinary url
+        required:true
+    },
+    thumbnail:{
+        type:String, //cloudinary url
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref : 'User'
+    }
+},{timestamps:true})
+
+export const Video = mongoose.model('Video',videoSchema)
