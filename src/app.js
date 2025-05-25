cd250666771ef5d74ae5aa8d275d977082c08619
Arijitttt <abhattacharya030@gmail.com>
2025-05-26 02:04:31 +0530

    updated playlist controller and router

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ import videoRouter from './routes/video.routes.js'
 import subscribeRouter from './routes/subscription.routes.js'
 import likeRouter from './routes/like.routes.js'
 import commentRouter from './routes/comment.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
 
 //routes
 app.use('/api/v1/healthcheck',healthcheckRouter)
@@ -34,6 +35,7 @@ app.use('/api/v1/video',videoRouter)
 app.use('/api/v1/subscription',subscribeRouter)
 app.use('/api/v1/like',likeRouter)
 app.use('/api/v1/comment',commentRouter)
+app.use('/api/v1/playlist',playlistRouter)
 
 export {errorHandler}
 export {app}
